f18d28e82f5b2a57bc6cfac9940c1dc94af42bc2
mleal <mleal@cs-pc41-192>
2019-09-03 16:30:24 -0500

    test 6

---
+++ test/src/javaTest.java
@@ -0,0 +1,9 @@
+
+public class javaTest {
+
+	public static void main(String[] args) {
+		int x=4;
+
+	}
+
+}
---
+++ test/src/test/javaTest.java
@@ -0,0 +1,10 @@
+package test;
+
+public class javaTest {
+
+	public static void main(String[] args) {
+      // TODO Auto-generated method stub
+    int x=4;
+	}
+
+}
